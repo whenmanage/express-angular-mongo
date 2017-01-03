5e4be5aee56b48f62cae25335be1283cbf1e01ed
Frank Denzer <frank.denzer@exco.de>
2017-01-03 15:42:00 +0100

    it works!

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
 /*eslint-env node */
 
 // set up ======================================================================
-var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var port  	 = process.env.PORT || 9000; 
-var mongoose = require('mongoose'); 					// mongoose for mongodb				// set the port
-var database = require('./config/database'); 			// load the database config
+var express = require('express');
 
-var morgan = require('morgan'); 		// log requests to the console (express4)
-var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+// create our app w/ express
+var app = express();
+
+// set the port
+var port = process.env.PORT || 9000;
+
+// mongoose for mongodb
+var MongoPortable = require("mongo-portable");
+// Instantiates a new object by passing a name
+var db = new MongoPortable("exampleDb");
+var mongoose = require('mongoose');
+
+// load the database config
+var database = db; //require('./config/database');
+
+// log requests to the console (express4)
+var morgan = require('morgan');
+// pull information from HTML POST (express4)
+var bodyParser = require('body-parser');
+// simulate DELETE and PUT (express4)
+var methodOverride = require('method-override');
 
 // configuration ===============================================================
-try{
-    mongoose.connect(database.url);
-}catch (err)
-{
-    console.log(err);
+try {
+  mongoose.connect(database.url);
+} catch (err) {
+  console.log(err);
 }// connect to mongoDB database on modulus.io
 
-app.use(express.static(__dirname + '/public')); 				// set static path
-app.use(morgan('dev')); 										// log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); 									// parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+// set static path
+app.use(express.static(__dirname + '/public'));
+
+// log every request to the console
+app.use(morgan('dev'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
-// routes 
+// routes
 require('./app/routes.js')(app);
 
 
